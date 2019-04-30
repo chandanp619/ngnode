7a960b8f4be04fe79768d499115be9ff514f5191
Chandan Pradhan <chandanp619@gmail.com>
2019-04-30 12:11:22 +0530

    Revert "Just a test"

--- app.js
+++ app.js
@@ -34,8 +34,6 @@ app.use(function(req, res, next) {
   next(createError(404));
 });
 
-
-
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
